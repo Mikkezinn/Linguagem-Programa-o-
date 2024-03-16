797da62e7a7c4d28c0ea5457069aca0d56a4151c
Abiny Magalhães <162757097+Mikkezinn@users.noreply.github.com>
2024-03-15 21:44:12 -0300

    Create Atividade01.java

---
+++ Atividade01.java
@@ -0,0 +1,15 @@
+package aula02;
+
+public class exercicio01 {
+   public static void main (String [] args) {
+	   //declarando as variaveis
+	   String sexo ="M";
+	   String nome = "Abiny Mikael";
+	   int idade = 18;
+			   
+			  //imprimir no console 
+			   System.out.println("Sexo: " + sexo);
+	           System.out.println("Nome: " + nome);
+	           System.out.println("Idade: " + idade);
+   }
+}
