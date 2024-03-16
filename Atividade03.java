1af9ab107f605302d1e5a560115fcabc001a29d3
Abiny Magalhães <162757097+Mikkezinn@users.noreply.github.com>
2024-03-15 21:42:37 -0300

    Create Atividade03.java

---
+++ Atividade03.java
@@ -0,0 +1,15 @@
+package aula02;
+
+public class exercicio3 {
+	  public static void main(String [] args) {
+		  String nome = "Abiny";
+		  String cpf = "14445866824";
+		  String cep = "88888888888";
+		  String cidade = "Curitiba";
+		  String estado = "Parana";
+		  
+		  System.out.println("O Nome da pessoa é " + nome + ", seu cpf é " + cpf + ", ele mora na cidade de " + cidade + ", no estado do " + estado);
+				  
+	  }
+
+}
