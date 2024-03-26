8f641d38868ed9d620bd1714a2d45dee55baad81
Abiny Magalhães <162757097+Mikkezinn@users.noreply.github.com>
2024-03-26 20:45:34 -0300

    Exercicio02.java

---
+++ Exercicio02.java
@@ -0,0 +1,31 @@
+package Desafios;
+import java.util.Scanner;
+
+public class Desafio02 {
+	public static void main (String [] args) {
+		
+		Scanner scanner = new Scanner (System.in);
+		System.out.print("Digite um número inteiro: ");
+		
+		int numero = scanner.nextInt();
+		
+		
+				if (numero % 2 == 0) {
+					
+					System.out.println(numero + " é um número par ");
+					
+					} else {
+						
+						System.out.println(numero + " é um número impar " );
+					}
+				
+		
+		scanner.close();
+		
+	}
+	
+	
+	
+	
+	
+}
