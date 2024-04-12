8490368afb8cd3ce21f2318b43cca76ea2e853db
Abiny Magalhães <162757097+Mikkezinn@users.noreply.github.com>
2024-04-11 21:45:47 -0300

    RADIO.java

--- RADIO.java
+++ RADIO.java
@@ -1,59 +1,48 @@
-package EXERCICIO;
+package radioteste;
+import java.util.Scanner;
+public class Radio {
+private boolean ligado;
+private int volume;
 
-public class RADIO {
+public Radio() {
+	
+	this.ligado = false;
+	this.volume = 50; //inicializa o radio em 50% de volume
+}
+	public void ligar() {
+		this.ligado = true;
+		System.out.println("O radio esta ligado");
+		}
+	public void aumentarVolume() {
+		if (ligado) {
+			if(volume < 100) {
+				volume = 100; //limite maximo do volume
+			}
+			System.out.println("Volume aumentado" + volume + "%");
+					
+	}else {
+		System.out.println("o volume esta no maximo");
+	}
+	}else{
+					System.out.println("Ligue o radio para ajustar o volume");
+	}
 
-	private int volume;
-    private boolean ligado;
 
-    public RADIO() {
-        this.volume = 50; 
-        this.ligado = false;
-    }
 
-    public void ligar() {
-        this.ligado = true;
-        System.out.println("O Radio está ligado.");
-    }
+public static void main (String[] args) {
+	
+Radio radio = new Radio ();
+Scanner scanner = new Scanner(System.in);
 
-    public void desligar() {
-        this.ligado = false;
-        System.out.println("O Radio está desligado.");
-    }
-
-    public void aumentarVolume() {
-        if (this.ligado) {
-            if (this.volume < 100) {
-                this.volume++;
-                System.out.println("Volume aumentado para " + this.volume + "%.");
-            } else {
-                System.out.println("Volume já está no máximo (100%).");
-            }
-        } else {
-            System.out.println("O rádio está desligado.");
-        }
-    }
-
-    public void diminuirVolume() {
-        if (this.ligado) {
-            if (this.volume > 1) {
-                this.volume--;
-                System.out.println("Volume diminuído para " + this.volume + "%.");
-            } else {
-                System.out.println("Volume já está no mínimo (1%).");
-            }
-        } else {
-            System.out.println("O rádio está desligado.");
-        }
-    }
-
-    public static void main(String[] args) {
-        RADIO radio = new RADIO();
-        radio.ligar();
-        radio.aumentarVolume();
-        radio.diminuirVolume();
-        radio.aumentarVolume();
-        radio.aumentarVolume();
-        radio.desligar();
-    }
+for(;;) {
+	System.out.println("\nMENU:");
+	System.out.println("1 - Ligar Radio");
+	System.out.println("2 - Desligar Radio");
+	System.out.println("3 - Aumentar Volume");
+	System.out.println("4 - Diminuir Volume"");
+	System.out.println("5 - Sair do programa");
+	System.out.println("Escolha uma opção");
 }
 
+}
+}
